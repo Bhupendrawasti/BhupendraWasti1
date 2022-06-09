a1a29e9ea6f02d50ad09a6d2a99873a0e788a293
Bhupendra wasti <102256635+Bhupendrawasti@users.noreply.github.com>
2022-06-09 23:17:18 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta name="viewport" content= "width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">    
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    
+        <title>Bhupendra Wasti</title>
+    </head>
+    <body>
+    
+    
+   <!--Header-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background: skyblue;">
+
+<div class="container-fluid">
+
+<div>
+
+<a href="/index.php" class="navbar-brand logo"><!--<img src="/img/logo.png" width="160" alt="Bhupendra Wasti">--> <h4>Bhupendra Wasti</h4></a>
+<style>
+h4{ 
+   font-family: 'Carter One', cursive;}
+   
+body {
+    background: rgb(238,174,202);
+background: -moz-radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+background: -webkit-radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#eeaeca",endColorstr="#94bbe9",GradientType=1);
+};
+
+</style>
+
+</div>
+
+<button class="navbar-toggler" data-toggle="collapse" data-target="#togglemenu" type="button">
+
+<span class="navbar-toggler-icon"></span>
+
+</button>
+<div class="collapse navbar-collapse" id="togglemenu">
+
+<ul class="navbar-nav ml-auto">
+
+<li class="nav-item">
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link"><span class="fa fa-home"> </span> Home</a>
+</li>
+
+<li class="nav-item">
+<a href="#" class="nav-link"><span class="fa fa-photo"></span> Gallary</a>
+</li>
+
+<li class="nav-item">
+<a href="#" class="nav-link"><span class="fa fa-play"></span> Games</a>
+</li>
+
+<li class="nav-item">
+<a href="#" class="nav-link"><span class="fa fa-wrench"></span> Online Tools</a>
+</li>
+
+<li class="nav-item">
+<a href="#" class="nav-link"><span class="fa fa-mobile"></span> Contact</a>
+</li>
+
+
+
+
+</ul>
+</div>
+</div>
+</nav>
+<br><br><br>
+<!--Header-->
+
+ <iframe src="https://bhupendrawasti.github.io/bhupendra-wasti/" width="100%" height="650" style="border:0;" allowfullscreen="" ></iframe>
+     
+ 
+    
+ 
+    </body>
+</html>
