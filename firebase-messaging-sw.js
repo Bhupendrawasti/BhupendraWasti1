1d8a38b0d2603f34b663557f334090f7ef157a85
Bhupendra wasti <102256635+Bhupendrawasti@users.noreply.github.com>
2026-06-11 00:45:05 +0545

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/12.14.0/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDO1...",
+  authDomain: "bhupendra-pwa.firebaseapp.com",
+  projectId: "bhupendra-pwa",
+  storageBucket: "bhupendra-pwa.firebasestorage.app",
+  messagingSenderId: "852947519760",
+  appId: "1:852947519760:web:e99c3e26c93912b8aa7d95"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body
+  });
+});
